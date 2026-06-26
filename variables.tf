4642fff73fd9579cd3f8618ff50186c5c5fa59e6
Aditya <adityasi127@gmail.com>
2026-06-27 05:11:35 +0530

    Remove default variables value

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "project_name" {
   description = "The name of the project"
   type        = string
-  default     = "aws_terraform_infrastructure"
 }
