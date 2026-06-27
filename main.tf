5cec7bd298a7b80a699cc49d1efe4a4ea5ba2681
Aditya <adityasi127@gmail.com>
2026-06-27 18:11:35 +0530

    Add Subnets

--- main.tf
+++ main.tf
@@ -10,3 +10,30 @@ resource "aws_vpc" "vpc" {
     Project = var.project_name
   }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_block
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name    = "${var.project_name}-public-subnet"
+    Project = var.project_name
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_cidr_block
+  map_public_ip_on_launch = false
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name    = "${var.project_name}-private-subnet"
+    Project = var.project_name
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "vpc_arn" {
   description = "ARN of project VPC"
   value       = aws_vpc.vpc.arn
 }
+
+output "public_subnet_id" {
+  description = "ID of public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "ID of private subnet"
+  value       = aws_subnet.private_subnet.id
+}
