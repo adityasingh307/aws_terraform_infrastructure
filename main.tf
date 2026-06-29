0dc15d8e8ef3aac0b8a43b80189ef10d8c926493
Aditya <adityasi127@gmail.com>
2026-06-29 10:29:03 +0530

    Add IGW,RT

--- main.tf
+++ main.tf
@@ -37,3 +37,26 @@ resource "aws_subnet" "private_subnet" {
     Project = var.project_name
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project_name}-igw"
+    Project = var.project_name
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
